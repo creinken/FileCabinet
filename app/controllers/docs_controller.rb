4220ac5d6fe9fab7dbd862a749086b36b8fa3aaf
creinken <reinkendigital@gmail.com>
2019-05-15 20:43:11 -0500

    Association between User and Files

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -8,11 +8,11 @@ before_action :find_doc, only: [:show, :edit, :update, :destroy]
     end
 
     def new
-        @doc = Doc.new
+        @doc = current_user.docs.build
     end
 
     def create
-        @doc = Doc.new(doc_params)
+        @doc = current_user.docs.build(doc_params)
 
         if @doc.save
             redirect_to @doc
